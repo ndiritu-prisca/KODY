f7767ed654cb36a276a582dd1ac4ecadac3f75c9
Epicsociety <measkswho@gmail.com>
2023-06-26 14:43:20 +0300

    need login_required

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, current_app, redirect, url_for
+from flask import Blueprint, render_template, flash, redirect, request, current_app, redirect, url_for
 import sqlite3
 from flask_mail import Message, Mail
 #from flask_login import login_required, current_user
@@ -31,24 +31,26 @@ def contact_us():
     #from . import login_manager
 
     if request.method == 'POST':
-        name = request.form['name']
-        email = current_user.email  # Get the email of the currently logged-in user
-        phone = request.form['phone']
-        subject = request.form['subject']
-        message = request.form['message']
+        #name = request.form['name']
+        #email = current_user.email  # Get the email of the currently logged-in user
+        #phone = request.form['phone']
+        #subject = request.form['subject']
+        #message = request.form['message']
 
-        mail.init_app(current_app)
+        #mail.init_app(current_app)
 
-        msg = Message(
-            subject="New contact form submission - {}".format(subject),
-            sender=email,
-            recipients=['transcriberandwriter@gmail.com']
-        )
+        #msg = Message(
+           # subject="New contact form submission - {}".format(subject),
+          #  sender=email,
+         #   recipients=['transcriberandwriter@gmail.com']
+        #)
 
-        msg.body = f"Name: {name}\nEmail: {email}\nPhone: {phone}\n\n{message}"
+        #msg.body = f"Name: {name}\nEmail: {email}\nPhone: {phone}\n\n{message}"
 
-        mail.send(msg)
+        #mail.send(msg)
 
-        return "Thank you for your message. We will get back to you shortly."
+        flash('Thank you for your message. We will get back to you shortly.')
+
+        return redirect(url_for('views.home'))
 
     return render_template("contact.html")
